29f26840d19faa1f00422160fca9660698983c15
dvishist <54056185+dvishist@users.noreply.github.com>
2020-05-18 06:13:40 +1000

    18/5/2020

--- mongodb.js
+++ mongodb.js
@@ -14,20 +14,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
 	const db = client.db(dbName)
 
-	db.collection('users').updateOne(
-		{
-			_id: new ObjectID('5ec17535a3a5d3497cb09a11')
-		},
-		{
-			$set: {
-				name: 'Baburao'
-			}
-		}
-	).then(result => {
-		console.log(result)
-	}).catch((e) => {
-		console.log(e)
-	})
-
+	db.collection('task').deleteOne({ description: 'Buy groceries' })
+		.then(result => console.log(result))
+		.catch(e => console.log(e))
 
 })
