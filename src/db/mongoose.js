ed4bb53ee597ffe7910944a3873e2ac737c351d2
dvishist <54056185+dvishist@users.noreply.github.com>
2020-05-19 04:51:10 +1000

    add API endpoints for create and read users and tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,60 +1,8 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true
 })
-
-const User = mongoose.model('User', {
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	email: {
-		type: String,
-		required: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) throw new Error('Email is invalid')
-		}
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value) {
-			if (value < 0) throw new Error('Age cannot be negative')
-		}
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 7,
-		validate(value) {
-			if (value.toLowercase().includes('password')) throw new Error('Invalid Password')
-		}
-	}
-})
-
-const Task = mongoose.model('Task', {
-	description: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	completed: {
-		type: Boolean,
-		default: false
-	}
-})
-
-const task1 = new Task({
-	description: '     learn VUE       ',
-})
-
-task1.save()
-	.then(() => console.log(task1))
-	.catch(e => console.log(e))
--- src/index.js
+++ src/index.js
@@ -1,8 +1,57 @@
 const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = new express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+	const user = new User(req.body)
+	user.save()
+		.then(() => res.status(201).send(user))
+		.catch(e => res.status(400).send(e))
+})
+
+app.get('/users', (req, res) => {
+	User.find({})
+		.then(users => res.send(users))
+		.catch(e => res.status(500).send())
+})
+
+app.get('/users/:id', (req, res) => {
+	const userId = req.params.id
+	User.findById(userId)
+		.then(user => {
+			return user ? res.send(user) : res.status(404).send()
+		})
+		.catch(e => res.status(500).send())
+})
+
+app.post('/tasks', (req, res) => {
+	const task = new Task(req.body)
+	task.save()
+		.then(() => res.status(201).send(task))
+		.catch(e => res.status(400).send(e))
+})
+
+app.get('/tasks', (req, res) => {
+	Task.find({})
+		.then(tasks => res.send(tasks))
+		.catch(e => res.status(500).send())
+})
+
+app.get('/tasks:id', (req, res) => {
+	const taskId = req.params.id
+	Task.findById(taskId)
+		.then(task => {
+			return task ? res.send(task) : res.status(404).send()
+		})
+		.catch(e => res.status(500).send())
+})
+
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+	console.log('Server is up on port ' + port)
 })
