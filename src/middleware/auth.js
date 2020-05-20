c2c9585af8aa0dde05e2bbf7c199eacec4556873
dvishist <54056185+dvishist@users.noreply.github.com>
2020-05-21 05:29:31 +1000

    add authentication for loading self profile

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,17 @@
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 const auth = async (req, res, next) => {
-    console.log("AUth middleware")
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'signInUser')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        if (!user) throw new Error()
+        req.user = user
+        next()
+    } catch (e) {
+        res.status(401).send({ error: 'Not Authenticated' })
+    }
 }
 
 
